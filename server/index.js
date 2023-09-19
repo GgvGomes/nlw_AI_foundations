bda2d29286f5076f97ebfa047d69632dd4857d20
Gabriel Gomes <ggvgabriel05@gmail.com>
2023-09-19 08:48:24 -0300

    testando para rodar o backend

--- server/index.js
+++ server/index.js
@@ -10,31 +10,39 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+// Fazer com que o Node sirva os arquivos do app em React criado
+app.use(express.static(path.resolve(__dirname, '../client/build')));
+
 app.get('/sumary/:id', async (request, response) => {
-    try{
+    try {
         await download(request.params.id, request.query.isShorts);
         const audioConverted = await convert();
-    
+
         const result = await transcribe(audioConverted);
-    
+
         // response.send("ID do vídeo " + request.params.id)
         return response.json({ result });
-    }catch(error){
+    } catch (error) {
         console.log(error)
         return response.status(400).json({ result: error.message });
     }
 });
 
 app.post('/sumary', async (request, response) => {
-    try{
+    try {
         const result = await summarize(request.body.text)
-    
+
         return response.json({ result });
     }
-    catch(error){
+    catch (error) {
         console.log(error)
         return response.status(400).json({ result: error.message });
     }
 })
+
+// Todas as outras solicitações GET não tratadas retornarão nosso app em React
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
+});
 
 app.listen(3333, () => console.log('Server running on port 3333!'));
